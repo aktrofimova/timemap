9875817ff3232c7ca9d42bad95043214e6273a03
aktrofimova <aktrofimova@gmail.com>
2020-03-12 16:54:25 +0200

    add cors support on backend

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+
+require 'rack/cors'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -33,5 +35,12 @@ module Timemap
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
