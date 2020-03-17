10c223231e80cbf4ed31c762d220641a90df17be
aktrofimova <aktrofimova@gmail.com>
2020-03-17 22:55:34 +0200

    restyle sign up page with mui and refactore to be a class component

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  TextField
+} from "@material-ui/core";
+
+const projects = [
+  {
+    value: "retex",
+    label: "Retex"
+  },
+  {
+    value: "spectar",
+    label: "SpectAR"
+  },
+  {
+    value: "martin-bros",
+    label: "Martin Brothers"
+  }
+];
+
+const positions = [
+  {
+    value: "dev",
+    label: "Developer"
+  },
+  {
+    value: "qa",
+    label: "Quality Assurance"
+  },
+  {
+    value: "pm",
+    label: "Project Manager"
+  }
+];
 
 class Signup extends Component {
   state = {
+    firstName:'',
+    lastName: '',
     email: '',
     password: '',
-    password_confirmation: '',
+    project: '',
+    position: '',
+
+    passwordConfirmation: '',
     errors: ''
   };
 
   handleChange = (event) => {
-    const {name, value} = event.target
+    const {id, value} = event.target
     this.setState({
-      [name]: value
+      [id]: value
     })
+  };
+
+  handleProjectChange = event => {
+    this.setState({project: event.target.value})
+  };
+
+  handlePositionChange = event => {
+    this.setState({position: event.target.value})
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const {email, password, password_confirmation} = this.state
+    const {firstName, lastName, email, password, passwordConfirmation} = this.state
     let user = {
+      first_name: firstName,
+      last_name: lastName,
       email: email,
       password: password,
-      password_confirmation: password_confirmation
+      password_confirmation: passwordConfirmation
     }
     axios.post('http://localhost:3001/users', {user}, {withCredentials: true})
       .then(response => {
@@ -56,36 +109,85 @@ class Signup extends Component {
   };
 
   render() {
-    const {email, password, password_confirmation} = this.state
+    const customSelect = {
+      width: '204px',
+      textAlign: 'left'
+    };
     return (
       <div>
         <h1>Sign Up</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="email"
-            type="text"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password confirmation"
-            type="password"
-            name="password_confirmation"
-            value={password_confirmation}
-            onChange={this.handleChange}
-          />
+        <form autoComplete="off" onSubmit={this.handleSubmit}>
+          <div>
+            <TextField required variant="outlined" id="firstName" label="First Name" onChange={this.handleChange}/>
+            <TextField required variant="outlined" id="lastName" label="Last Name" onChange={this.handleChange}/>
+          </div>
 
-          <button placeholder="submit" type="submit">
-            Sign Up
-          </button>
+          <div>
+            <div>
+              <TextField select id="selectProject" style={customSelect}
+                         label="Select Project"
+                         value={this.state.project}
+                         onChange={this.handleProjectChange}
+                         variant="outlined">
+                {projects.map(option => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+
+              <TextField select id="selectPosition" style={customSelect}
+                         label="Select Position"
+                         value={this.state.position}
+                         onChange={this.handlePositionChange}
+                         variant="outlined"
+              >
+                {positions.map(option => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </div>
+          </div>
+
+          <div>
+            <TextField required variant="outlined" type="email" id="email" label="E-Mail" onChange={this.handleChange}/>
+          </div>
+
+          <div>
+            <FormControl required variant="outlined">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                id="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                labelWidth={85}
+              />
+            </FormControl>
+
+            <FormControl required variant="outlined">
+              <InputLabel htmlFor="passwordConfirmation">Confirm Password</InputLabel>
+              <OutlinedInput
+                id="passwordConfirmation"
+                type="password"
+                value={this.state.password_confirmation}
+                onChange={this.handleChange}
+                labelWidth={140}
+              />
+            </FormControl>
+          </div>
+
+          <div>
+
+          </div>
+
+          <div>
+            <Button variant="outlined" color="secondary">Cancel</Button>
+            <Button variant="outlined" color="primary" type="submit">Sign Up</Button>
+          </div>
+
 
         </form>
         <div>{this.state.errors ? this.handleErrors() : null}</div>
