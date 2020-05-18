ab2e3734413a9a8e19b442d65943312e285b602a
aktrofimova <aktrofimova@gmail.com>
2020-05-18 22:14:14 +0300

    fic ui errors for NewTask component

--- client/src/components/NewTask.js
+++ client/src/components/NewTask.js
@@ -4,52 +4,45 @@ import { Button, FormControl, InputLabel, MenuItem, OutlinedInput, TextField } f
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers';
 
+const NewTask = () => {
+  const tasks = [{value: 'dev', label: 'Development'},
+    {value: 'qa', label: 'Testing'},
+    {value: 'ot', label: 'Overtime Hours'},
+  ]
 
-const tasks = [{value: 'dev', label: 'Development'},
-  {value: 'qa', label: 'Testing'},
-  {value: 'ot', label: 'Overtime Hours'},
-]
+  const [project, setProject] = React.useState(''),
+    [task, setTask] = React.useState(''),
+    [hours, setHours] = React.useState(''),
+    [details, setDetails] = React.useState(''),
+    [selectedDate, setSelectedDate] = React.useState(new Date()),
+    [startedAt, setStartTime] = React.useState(new Date()),
+    [endedAt, setEndTime] = React.useState(new Date());
 
-class NewTask extends Component {
-  state = {
-    project: "First Project",
-    task: '',
-    started_at: new Date(),
-    ended_at: new Date(),
-    date: '',
-    hours: ''
-  }
-
-  componentDidMount() {
-
-  }
-
-  handleChange = (event) => {
-    const {id, value} = event.target
-    this.setState({
-      [id]: value
-    })
+  const handleDetailsChange = event => {
+    setDetails(event.target.value);
   };
 
-  handleTaskChange = event => {
-    this.setState({task: event.target.value})
+  const handleTaskChange = event => {
+    setTask(event.target.value);
   };
 
-  handleStartTimeChange = event => {
-    this.setState({started_at: event.getTime()});
+  const handleHoursChange = event => {
+    setHours(event.target.value);
   };
 
-  handleEndTimeChange = event => {
-    this.setState({ended_at: event.getTime()});
+  const handleStartTimeChange = startedAt => {
+    setStartTime(startedAt);
   };
 
-  handleDateChange = event => {
-    this.setState({date: event.target.value});
+  const handleEndTimeChange = endedAt => {
+    setEndTime(endedAt);
   };
 
+  const handleDateChange = date => {
+    setSelectedDate(date);
+  };
 
-
-  getDate = (rawDate) => {
+  const getDate = (rawDate) => {
     var date = rawDate || new Date();
     var dd = String(date.getDate()).padStart(2, '0');
     var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -58,70 +51,73 @@ class NewTask extends Component {
     return mm + '/' + dd + '/' + yyyy;
   }
 
-  getDateInWords = (rawDate) => {
+  const getDateInWords = (rawDate) => {
     var date = new Date(rawDate) || new Date();
     var dateArr = date.toDateString().split(' ');
     return dateArr[2] + ' ' + dateArr[1] + ' ' + dateArr[3];
   }
 
+  return (
+    <div className="new_task">
+      <TextField className="new_task_field new_task_select"
+                 select id="selectTask"
+                 label="Select Task"
+                  // value={this.state.task}
+                 onChange={handleTaskChange}>
+        {tasks.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {/*Project Name: {option.label}*/}
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
 
-  render() {
-    return (
-      <div className="new_task">
-        <TextField className="new_task_field new_task_select"
-                   select id="selectTask"
-                   label="Select Task"
-                   // value={this.state.task}
-                   onChange={this.handleTaskChange}>
-          {tasks.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {/*Project Name: {option.label}*/}
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+      <TextField id="details" className="new_task_field new_task_details" label="Details" onChange={handleDetailsChange}/>
 
-        <TextField id="details" className="new_task_field new_task_details" label="Details" onChange={this.handleChange}/>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardTimePicker
+          id="startedAt"
+          className="new_task_field new_task_time"
+          label="Started At"
+          value={startedAt}
+          onChange={handleStartTimeChange}
+        />
 
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardTimePicker
+          id="endedAt"
+          className="new_task_field new_task_time"
+          label="Ended At"
+          value={endedAt}
+          onChange={handleEndTimeChange}
+        />
 
-          <KeyboardTimePicker
-            id="startedAt"
-            className="new_task_field new_task_time"
-            // style={{maxWidth: '250px'}}
-            label="Started At"
-            // placeholder={this.state.started_at}
-            onChange={this.handleStartTimeChange}
-          />
+        <KeyboardDatePicker
+          className="new_task_field new_task_date"
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          // format="MMM dd, yyyy"
+          id="date"
+          label="Date"
+          value={selectedDate}
+          onChange={handleDateChange}
+        />
+      </MuiPickersUtilsProvider>
 
-          <KeyboardTimePicker
-            id="endedAt"
-            className="new_task_field new_task_time"
-            label="Ended At"
-            // placeholder={this.state.started_at}
-            onChange={this.handleEndTimeChange}
-          />
+      <TextField id="hours" className="new_task_field new_task_hours" label="Hours" defaultValue="00:00" onChange={handleHoursChange}/>
 
-          <KeyboardDatePicker
-            className="new_task_field new_task_date"
-            disableToolbar
-            variant="inline"
-            format="MMM dd, yyyy"
-            id="date"
-            label="Date"
-            value={this.getDate()}
-            onChange={this.handleDateChange}
-          />
+      <Button style={{maxHeight: '48px', width: '10%'}} variant="outlined">Add Task</Button>
 
-        </MuiPickersUtilsProvider>
+    </div>
 
-        <TextField id="hours" className="new_task_field new_task_hours" label="Hours" defaultValue="00:00" onChange={this.handleChange}/>
-
-        <Button variant="outlined">Add Task</Button>
-
-      </div>
-    );
-  }
+  );
 }
 
 export default NewTask;
+
+
+
+
+
+
+
